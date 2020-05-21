52f52b072ca68a1a53e6b9d250b6b8e12a1e25cd
雲豹ΦωΦ <armfire1911@protonmail.com>
2020-05-21 17:17:14 +0800

    bot game status update

--- Nishimi_Yayudzuru.js
+++ Nishimi_Yayudzuru.js
@@ -1,14 +1,19 @@
 ﻿//啟動模組
 const Discord = require('discord.js');
 const client = new Discord.Client({ autoReconnect: true });
-//於cmd回傳啟動訊息
+//於cmd回傳啟動訊息&設定BOT自訂遊戲狀態
 client.on("ready", () => {
     //用於統計使用者
-    console.log(`結弦回家囉!接觸了 ${client.users.size} 位成員，看到了 ${client.channels.size} 個頻道，加入了 ${client.guilds.size} 個伺服器`);
-    client.user.setActivity(`陪ArmFire1911聊天|結弦help`);
-});
-client.on('ready', () => {
     console.log(`以 ${client.user.tag}身分登入了!`);
+    console.log(`結弦回家囉!接觸了 ${client.users.size} 位成員，看到了 ${client.channels.size} 個頻道，加入了 ${client.guilds.size} 個伺服器`);
+    //使用者狀態(線上online、閒置idle、請勿打擾dnd、隱形invisible)
+    client.user.setStatus('dnd')
+    client.user.setPresence({
+        game: {
+            name: 'Chatting with 雲豹ΦωΦ',
+            type: 'PLAYING',
+        }
+    });
 });
 
 //限制使用者使用的指令組
